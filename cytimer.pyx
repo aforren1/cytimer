17ac22267492979ae492bb180495c5243a7d03e4
Alex Forrence <alex.forrence@gmail.com>
2020-05-04 11:06:15 -0400

    Picklable timer

--- cytimer.pyx
+++ cytimer.pyx
@@ -3,19 +3,25 @@ from libcpp cimport bool
 cdef extern from "timer.hpp" namespace "mycpp":
     cdef cppclass CMonoClock "mycpp::MonoClock":
         CMonoClock() except +
-        CMonoClock(const bool relative) except +
+        CMonoClock(const bool relative, const long long ns) except +
         long long get_time_ns()
         double get_time()
+        long long dump_origin_ns()
 
 cdef class MonoClock:
     cdef CMonoClock cclk
-    def __cinit__(self, const bool relative=True):
-        self.cclk = CMonoClock(relative)
+    def __cinit__(self, const bool relative=True, ns=-1):
+        self.cclk = CMonoClock(relative, ns)
     cpdef get_time_ns(self):
         return self.cclk.get_time_ns()
     cpdef get_time(self):
         return self.cclk.get_time()
     cpdef getTime(self):
         return self.cclk.get_time()
+    cpdef dump_origin_ns(self):
+        return self.cclk.dump_origin_ns()
+    def __reduce__(self):
+        return (MonoClock, (True, self.dump_origin_ns()))
+
 
 mono_clock = MonoClock.__new__(MonoClock)
--- timer.hpp
+++ timer.hpp
@@ -3,6 +3,7 @@
 using std::chrono::nanoseconds;
 using std::chrono::steady_clock;
 using std::chrono::time_point;
+using std::chrono::duration_cast;
 
 namespace mycpp {
 
@@ -10,25 +11,36 @@ class MonoClock
 {
 public:
     MonoClock() {
-        reset_time = steady_clock::time_point();
+        reset_time = clock.now();
     }
-    MonoClock(const bool relative)
+    MonoClock(const bool relative, const long long ns = -1)
     {
-        reset_time = steady_clock::time_point();
-        if (relative)
-        {
-            reset_time = clock.now();
+        if (ns > -1) {
+            reset_time = steady_clock::time_point() + nanoseconds(ns);
+        }
+
+        else {
+            reset_time = steady_clock::time_point();
+            if (relative)
+            {
+                reset_time = clock.now();
+            }
         }
     }
 
     ~MonoClock() {}
     inline long long get_time_ns()
     {
-        return std::chrono::duration_cast<nanoseconds>(clock.now() - reset_time).count();
+        return duration_cast<nanoseconds>(clock.now() - reset_time).count();
     }
     inline double get_time()
     {
         return static_cast<double>(get_time_ns()) / 1000000000.0;
+    }
+
+    inline long long dump_origin_ns()
+    {
+        return duration_cast<nanoseconds>(reset_time.time_since_epoch()).count();
     }
 
 private:
